2cbef98ea6329eb40b8d9c419840cb1a06e3c509
love_chemistry <pinowcao@outlook.com>
2026-03-30 13:20:03 +0800

    beautify start menu

--- game.py
+++ game.py
@@ -5,44 +5,113 @@ from player import InputHandler
 from game_mode import GameMode, GameConfig
 
 
-def select_mode(stdscr):
-    stdscr.clear()
+ASCII_TITLE = [
+    "    ____               _  __     ______             ",
+    "   / __ \\ ____   ____ ( )/ /_   /_  __/____ _ ____  ",
+    "  / / / // __ \\ / __ \\|// __/    / /  / __ `// __ \\ ",
+    " / /_/ // /_/ // / / / / /_     / /  / /_/ // /_/ / ",
+    "/_____/ \\____//_/ /_/  \\__/    /_/   \\__,_// .___/  ",
+    "                                          /_/       ",
+    "    __   __             _       __ __     _  __           ______ _  __    ",
+    "   / /_ / /_   ___     | |     / // /_   (_)/ /_ ___     /_  __/(_)/ /___ ",
+    "  / __// __ \\ / _ \\    | | /| / // __ \\ / // __// _ \\     / /  / // // _ \\",
+    " / /_ / / / //  __/    | |/ |/ // / / // // /_ /  __/    / /  / // //  __/",
+    " \\__//_/ /_/ \\___/     |__/|__//_/ /_//_/ \\__/ \\___/    /_/  /_//_/ \\___/ ",
+]
+
+
+def get_centered_x(width, line_length):
+    return max(0, (width - line_length) // 2)
+
+
+def get_centered_y(height, total_lines):
+    return max(0, (height - total_lines) // 2)
+
+
+def draw_centered_lines(stdscr, lines, start_y=None):
+    height, width = stdscr.getmaxyx()
+    total_lines = len(lines)
+    if start_y is None:
+        start_y = get_centered_y(height, total_lines)
     
-    menu_lines = [
-        "================================",
-        "       别踩白块",
-        "================================",
+    for i, line in enumerate(lines):
+        y = start_y + i
+        if y < height:
+            x = get_centered_x(width, len(line))
+            max_len = width - x - 1
+            if y == height - 1:
+                max_len = width - x - 1
+            try:
+                display_line = line[:max_len] if len(line) > max_len else line
+                stdscr.addstr(y, x, display_line)
+            except curses.error:
+                pass
+
+
+def show_rules(stdscr):
+    stdscr.clear()
+    height, width = stdscr.getmaxyx()
+    
+    rules_lines = [
+        "========== 游戏规则 ==========",
         "",
-        "请选择游戏模式：",
+        "【基本操作】",
+        "  D - 点击第一列",
+        "  F - 点击第二列",
+        "  J - 点击第三列",
+        "  K - 点击第四列",
         "",
-        "[1] 无限模式",
-        "    规则：点击黑块得分，失误即结束",
+        "【无限模式】",
+        " 点击黑块得分",
+        "点错白块游戏结束",
         "",
-        "[2] 限时模式",
-        "    规则：60秒限时，连击加分，失误扣10分",
+        "【限时模式】",
+        "  60秒限时挑战",
+        "连击可获得加分倍率",
+        "失误扣分并中断连击",
         "",
-        "按 1 或 2 选择模式...",
-        "按 Q 退出游戏",
-        "================================",
+        "========== 游戏规则 ==========",
+        "按任意键返回...",
     ]
     
-    for i, line in enumerate(menu_lines):
-        try:
-            height, width = stdscr.getmaxyx()
-            if i < height:
-                max_len = width - 1
-                stdscr.addstr(i, 0, line[:max_len] if len(line) > max_len else line)
-        except curses.error:
-            pass
-    
+    draw_centered_lines(stdscr, rules_lines)
     stdscr.refresh()
     
+    stdscr.nodelay(False)
+    stdscr.getch()
+
+
+def select_mode(stdscr):
+    curses.curs_set(0)
+    
     while True:
+        stdscr.clear()
+        height, width = stdscr.getmaxyx()
+        
+        menu_lines = [
+            "",
+            "[1] 无限模式",
+            "[2] 限时模式",
+            "[R] 规则说明",
+            "[Q] 退出游戏",
+        ]
+        
+        all_lines = ASCII_TITLE + menu_lines
+        total_lines = len(all_lines)
+        start_y = get_centered_y(height, total_lines)
+        
+        draw_centered_lines(stdscr, ASCII_TITLE, start_y)
+        draw_centered_lines(stdscr, menu_lines, start_y + len(ASCII_TITLE))
+        
+        stdscr.refresh()
+        
         key = stdscr.getch()
         if key == ord('1'):
             return GameMode.INFINITE
         elif key == ord('2'):
             return GameMode.TIMED
+        elif key == ord('r') or key == ord('R'):
+            show_rules(stdscr)
         elif key == ord('q') or key == ord('Q'):
             return None
 
